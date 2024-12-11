a3fbd2adceadafd3ebad32f8a4d8508adb162bbe
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-11 11:45:28 +0100

    feat(projects): ProjectCard initial UI

---
+++ components/ProjectsPage/ProjectCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="text-base pb-4">
+      <span class="font-bold text-portfolio-secondary-purple">Project 1 </span>
+      <span class="text-base_true_gray">/ whobrings</span>
+    </div>
+    <div
+      class="relative flex flex-col justify-center bg-portfolio-primary-100 border border-lines rounded-xl overflow-hidden"
+    >
+      <div
+        class="absolute flex items-center justify-center top-3 right-3 bg-[#81D4AF] rounded p-1"
+      >
+        <UIcon name="ri:vuejs-line" class="w-5 h-5 text-base_true_black" />
+      </div>
+      <div class="border-b border-lines">
+        <NuxtImg src="./whobrings.png" alt="whobrings.png" />
+      </div>
+      <div class="px-8 py-6 text-base_true_gray">
+        Fullstack application for creating and managing event lists.
+      </div>
+      <div class="flex justify-start px-8 pb-6">
+        <button
+          class="bg-[#1C2B3A] text-sm text-base_true_white rounded-md py-2 px-3"
+        >
+          view-project
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -23,19 +23,26 @@
         </div>
       </div>
       <!-- FILTERS -->
-      <div class="flex flex-col gap-4 py-4">
-        <div
-          v-if="isOpen"
-          v-for="filter in filterData"
-          :key="filter.id"
-          class="pl-7"
-        >
+      <div v-if="isOpen" class="flex flex-col gap-4 pt-4">
+        <div v-for="filter in filterData" :key="filter.id" class="pl-7">
           <CheckBox
-            :model-value="selectedFilters[filter.id as keyof typeof selectedFilters]"
+            v-model="selectedFilters[filter.id as keyof typeof selectedFilters]"
             :icon="filter.icon"
             :id="filter.id"
           />
         </div>
+      </div>
+    </div>
+    <div>
+      <!-- PROJECTS -->
+      <div class="px-7 pb-4">
+        // projects
+        <span class="text-base_true_gray"
+          >/ {{ getSelectedFilterTitle() }}</span
+        >
+      </div>
+      <div class="px-7">
+        <ProjectCard />
       </div>
     </div>
   </div>
@@ -43,17 +50,36 @@
 
 <script setup lang="ts">
 import CheckBox from "~/components/Common/CheckBox.vue";
+import ProjectCard from "~/components/ProjectsPage/ProjectCard.vue";
 import { filterData } from "~/data";
 
 const isOpen = ref(false);
 const selectedFilters = reactive({
   react: false,
   vue: false,
+  nuxt: false,
   express: false,
   nest: false,
 });
 
 const toggleVisibility = (): void => {
   isOpen.value = !isOpen.value;
+};
+const getSelectedFilterTitle = () => {
+  let filters: string[] = [];
+  for (const [key, value] of Object.entries(selectedFilters)) {
+    if (value) {
+      filters.push(key);
+    }
+  }
+  if (filters.length === 0) {
+    return "all";
+  } else {
+    if (filters.length == Object.keys(selectedFilters).length) {
+      return "all";
+    } else {
+      return filters.join("; ");
+    }
+  }
 };
 </script>
