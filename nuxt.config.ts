121a9271a24d38658627c8776f3f836e44bc508c
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-13 22:48:19 +0100

    feat(config): enable network access for devServer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0", // Omogućava pristup sa mreže
+    port: 3000,
+  },
   modules: ["@nuxt/image", "@nuxt/ui", "@pinia/nuxt"],
   ssr: false, // Ovo će omogućiti SPA mod
   typescript: {
