84188aeecb860b73b8e6134dfd5b63563599a21b
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-16 11:05:50 +0100

    feat(contact-me): FindMe component

---
+++ components/ContactPage/FindMe.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col pb-10 text-base text-base_true_gray">
+    <div>
+      <div
+        class="bg-lines flex items-center gap-3 pl-7 py-2 text-base_true_white"
+        @click="toggleVisibility()"
+      >
+        <UIcon
+          v-if="!isOpen"
+          name="ri:arrow-right-s-fill"
+          class="w-5 h-5 text-base_true_white"
+        />
+        <UIcon
+          v-else
+          name="ri:arrow-down-s-fill"
+          class="w-5 h-5 text-base_true_white"
+        />
+        <p>find-me-also-in</p>
+      </div>
+    </div>
+    <div v-if="isOpen" class="flex flex-col pt-3">
+      <NuxtLink
+        to="https://www.instagram.com/neven_mitrovic/"
+        target="_blank"
+        class="flex items-center gap-2 pl-7 text-base"
+      >
+        <UIcon name="ri:instagram-fill" class="w-5 h-5" />
+        <span>Instagram accaunt</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+const toggleVisibility = (): void => {
+  isOpen.value = !isOpen.value;
+};
+</script>
--- pages/contact-me.vue
+++ pages/contact-me.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="bg-portfolio-primary-50 pb-10">
     <div class="py-5 px-7 text-sm">_contact-me</div>
+    <FindMe />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FindMe from "~/components/ContactPage/FindMe.vue";
+</script>
