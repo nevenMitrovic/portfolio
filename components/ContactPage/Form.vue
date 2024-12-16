a5a08f0293e7ef8dcddb5e02f1c0a4cbc7815c8e
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-16 17:50:41 +0100

    feat(mobile-version): Mobile version completed

--- components/ContactPage/Form.vue
+++ components/ContactPage/Form.vue
@@ -1,5 +1,9 @@
 <template>
-  <form class="flex flex-col gap-4 px-7">
+  <form
+    v-if="status === ''"
+    @submit.prevent="sendEmail"
+    class="flex flex-col gap-4 px-7"
+  >
     <Input :id="'name'" :name="'name'" v-model="name" :label="'_name'" />
     <Input :id="'email'" :name="'email'" v-model="email" :label="'_email'" />
     <div>
@@ -16,18 +20,93 @@
     <div>
       <button
         type="submit"
-        class="bg-[#1C2B3A] rounded-md text-sm text-base_true_white py-2 px-3"
+        class="flex gap-1 bg-[#1C2B3A] rounded-md text-sm text-base_true_white py-2 px-3"
+        :disabled="loading"
       >
-        submit-message
+        <div
+          v-if="loading"
+          class="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-base_true_gray mr-2"
+        ></div>
+        <span v-if="!loading">submit-message</span>
+        <span v-else>sending...</span>
       </button>
     </div>
   </form>
+  <div v-else-if="status === 'success'" class="flex flex-col gap-3 px-7">
+    <h2 class="text-base_true_white text-2xl">Thank you! 🤘</h2>
+    <p class="text-lg text-base_true_gray pb-2">
+      Your message has been accepted. You will recieve answer really soon!
+    </p>
+    <div class="flex justify-center items-center">
+      <button
+        type="button"
+        class="flex gap-1 bg-[#1C2B3A] rounded-md text-sm text-base_true_white py-2 px-3"
+        @click="() => (status = '')"
+      >
+        send-new-message
+      </button>
+    </div>
+  </div>
+  <div v-else="status === 'error'" class="flex flex-col gap-3 px-7">
+    <h2 class="text-base_true_white text-2xl">Error</h2>
+    <p class="text-lg text-base_true_gray pb-2">Please try again !</p>
+    <div class="flex justify-center items-center">
+      <button
+        type="button"
+        class="flex gap-1 bg-[#1C2B3A] rounded-md text-sm text-base_true_white py-2 px-3"
+        @click="() => (status = '')"
+      >
+        send-new-message
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import Input from "./Input.vue";
+import emailjs from "emailjs-com";
+
+const config = useRuntimeConfig();
 
 const name = ref<string>("");
 const email = ref<string>("");
 const message = ref<string>("");
+const status = ref<string>("");
+const loading = ref<boolean>(false);
+
+const resetForm = (): void => {
+  name.value = "";
+  email.value = "";
+  message.value = "";
+};
+const sendEmail = async () => {
+  if (loading.value) return;
+  loading.value = true;
+
+  try {
+    const serviceID = config.public.serviceID;
+    const templateID = config.public.templateID;
+    const userID = config.public.publicID;
+
+    await emailjs.send(
+      serviceID,
+      templateID,
+      {
+        from_name: name.value,
+        from_email: email.value,
+        message: message.value,
+      },
+      userID
+    );
+
+    status.value = "success";
+    resetForm();
+  } catch (error) {
+    console.error(error);
+    status.value = "error";
+  } finally {
+    loading.value = false;
+    console.log(status.value);
+  }
+};
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   devServer: {
-    host: "0.0.0.0", // Omogućava pristup sa mreže         
+    host: "0.0.0.0",
     port: 3000,
   },
+  runtimeConfig: {
+    public: {
+      serviceID: process.env.NUXT_PUBLIC_SERVICE_ID,
+      templateID: process.env.NUXT_PUBLIC_TEMPLATE_ID,
+      publicID: process.env.NUXT_PUBLIC_PUBLIC_KEY,
+    },
+  },
   modules: ["@nuxt/image", "@nuxt/ui", "@pinia/nuxt"],
-  ssr: false, // Ovo će omogućiti SPA mod
+  ssr: false,
   typescript: {
     typeCheck: true,
   },
