5b10f1d63cb887ded477bb5cf5576a2c5f981657
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-16 20:55:54 +0100

    feat(header): Header for all devices completed

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,14 +1,49 @@
 <template>
   <header
-    class="flex justify-between items-center border-b border-lines p-5 text-base_true_gray"
+    class="flex justify-between sm:justify-start items-center border-b border-lines h-[56px] px-5 sm:pl-5 text-base_true_gray"
   >
-    <!-- LOGO -->
-    <div class="text-base">neven-mitrovic</div>
-    <div v-if="commonStore.isMobile">
+    <div
+      class="flex items-center text-base md:border-r w-full md:w-3/12 border-lines h-full"
+    >
+      neven-mitrovic
+    </div>
+    <div v-if="commonStore.isMobile || commonStore.isTablet">
       <button class="flex items-center justify-center" @click="toggleMenu">
         <UIcon name="ri:menu-fill" class="w-5 h-5 hover:text-base_true_white" />
       </button>
     </div>
+    <nav v-else class="flex h-full w-full justify-between items-center">
+      <li class="list-none flex h-full">
+        <ul
+          v-for="link in navigationLinks.slice(0, 3)"
+          :key="link.title"
+          class="border-r border-lines h-full flex items-center"
+        >
+          <NuxtLink
+            :to="link.path"
+            class="py-4 px-5 hover:text-base_true_white hover:border-b-2 hover:border-portfolio-accent-orange"
+            exact-active-class="text-base_true_white"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </ul>
+      </li>
+      <div class="h-full">
+        <div
+          v-for="link in navigationLinks.slice(3, 4)"
+          :key="link.title"
+          class="border-l border-lines h-full flex items-center"
+        >
+          <NuxtLink
+            :to="link.path"
+            class="py-4 px-5 hover:text-base_true_white hover:border-b-2 hover:border-portfolio-accent-orange"
+            exact-active-class="text-base_true_white"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </div>
+      </div>
+    </nav>
   </header>
   <div v-if="isMenuOpen" class="relative">
     <nav class="absolute top-1 z-10 w-full bg-portfolio-primary-50">
