6f81f7921320b3708b809662838f3350efbabe43
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-18 18:27:53 +0100

    fix(memory-game): started with different logic for play

--- components/LandingPage/MemoryGame.vue
+++ components/LandingPage/MemoryGame.vue
@@ -4,14 +4,14 @@
       <h1 class="text-3xl font-bold mb-6">memory-game</h1>
       <div v-if="!endGame" class="grid grid-cols-4 gap-2">
         <button
-          v-for="(card, index) in cards"
+          v-for="(card, index) in cardsStorage"
           :key="index"
           @click="play(index)"
           :disabled="loading"
           :class="loading && 'cursor-wait'"
           class="w-[54px] h-[54px] p-4 border border-lines bg-inherit flex items-center justify-center text-base_true_white text-xs rounded-md"
         >
-          <UIcon v-if="card != ''" :name="card" class="w-5 h-5" />
+          <UIcon v-if="card.show" :name="card.icon" class="w-5 h-5" />
           <p v-else class="flex items-center justify-center">card</p>
         </button>
       </div>
@@ -26,15 +26,21 @@ interface StorageItemType {
   icon: string;
   value: number;
 }
+interface CardsStorageType {
+  index: number | null;
+  icon: string;
+  show: boolean;
+}
 const storage = ref<StorageItemType[]>([
   { icon: "ri:apple-fill", value: 0 },
-  { icon: "ri:amazon-fill", value: 0 },
+  { icon: "ri:amazon-fill", value: 0, },
   { icon: "ri:google-fill", value: 0 },
   { icon: "ri:android-fill", value: 0 },
   { icon: "ri:discord-line", value: 0 },
   { icon: "ri:javascript-fill", value: 0 },
 ]);
-const cards = ref<string[]>(Array(12).fill(""));
+const cardsStorage = ref<CardsStorageType[]>([]); // za pamcenje kartice
+const pickedCardIndex = ref<number | null>(null); // za uporedjivanje
 const endGame = ref<boolean>(false);
 const loading = ref<boolean>(false);
 
@@ -45,12 +51,6 @@ const play = (index: number): void => {
 
     const randomNumber = Math.floor(Math.random() * 6);
     if (storage.value[randomNumber].value == 2) continue;
-
-    storage.value[randomNumber].value = storage.value[randomNumber].value + 1;
-    console.log(storage.value[randomNumber].value, randomNumber);
-    cards.value[index] = storage.value[randomNumber].icon;
-    loading.value = false;
-    break;
   }
 };
 const checkItemsValue = (): boolean => {
