d557d41633cf4d4c57d8fde6c13e62059be2cd2c
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-20 14:51:32 +0100

    feat(memory-game): game completed with timer

--- components/LandingPage/MemoryGame.vue
+++ components/LandingPage/MemoryGame.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="relative border border-lines rounded-lg p-8 bg-portfolio-primary-100">
-    <div class="relative flex flex-col items-center justify-center rounded-sm">
+  <div
+    class="relative border border-lines rounded-lg p-8 bg-portfolio-primary-100 w-[306px] h-[368px]"
+  >
+    <div
+      class="w-full h-full flex flex-col items-center justify-center rounded-sm"
+    >
       <h1 class="text-3xl font-bold mb-6">memory-game</h1>
+      <div class="absolute right-3 top-2">{{ time }}</div>
       <div v-if="!endGame" class="grid grid-cols-4 gap-2">
         <button
           v-for="(card, index) in cardsStorage"
@@ -15,10 +20,20 @@
           <p v-else class="flex items-center justify-center">card</p>
         </button>
       </div>
-      <div v-else>END GAME</div>
-      <button @click="resetGame" class="mt-6 bg-[#1C2B3A] rounded-md py-2 px-3">
-        Reset
-      </button>
+      <div v-else class="h-full w-full flex items-center justify-center">
+        END GAME
+      </div>
+      <div class="flex item-center justify-between gap-4">
+        <button @click="timer" class="mt-6 bg-[#1C2B3A] rounded-md py-2 px-3">
+          {{ endGame ? "restart" : "start" }}
+        </button>
+        <button
+          @click="restartGame"
+          class="mt-6 bg-[#1C2B3A] rounded-md py-2 px-3"
+        >
+          end
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +58,9 @@ const cardsStorage = ref<CardType[]>(
 );
 const lastPickedCardIndex = ref<number | null>(null);
 const endGame = ref<boolean>(false);
+const startGame = ref<boolean>(true);
 const loading = ref<boolean>(false);
+const time = ref<string>("00:00");
 
 const play = (index: number): void => {
   if (!cardsStorage.value[index].show) {
@@ -62,7 +79,8 @@ const play = (index: number): void => {
         return;
       }
 
-      const randomIcon = availableIcons[Math.floor(Math.random() * availableIcons.length)];
+      const randomIcon =
+        availableIcons[Math.floor(Math.random() * availableIcons.length)];
       cardsStorage.value[index].icon = randomIcon;
     }
 
@@ -98,12 +116,47 @@ const play = (index: number): void => {
     loading.value = false;
   }
 };
-const resetGame = (): void => {
+const restartGame = (): void => {
   cardsStorage.value = Array(12)
     .fill(null)
     .map(() => ({ icon: "", show: false }));
   lastPickedCardIndex.value = null;
-  endGame.value = false;
+  startGame.value = true;
+  endGame.value = true;
   loading.value = false;
+};
+const timer = (): void => {
+  if (startGame.value) {
+    startGame.value = false;
+    endGame.value = false;
+    let sec: number | string = 0;
+    let min: number | string = 0;
+    const counter = setInterval(() => {
+      if (typeof sec === "string") sec = parseInt(sec) + 1;
+      if (typeof sec === "number") {
+        if (sec > 59) {
+          sec = 0;
+          if (typeof min === "string") min = parseInt(min) + 1;
+          if (typeof min === "number")
+            min = min < 10 ? "0" + min : min.toString();
+          if (typeof sec === "number")
+            sec = sec < 10 ? "0" + sec : sec.toString();
+        }
+        if (typeof sec === "number")
+          sec = sec < 10 ? "0" + sec : sec.toString();
+        if (typeof min === "number")
+          min = min < 10 ? "0" + min : min.toString();
+      }
+      time.value = `${min}:${sec}`;
+      if (endGame.value) {
+        sec = 0;
+        min = 0;
+        startGame.value = true;
+        clearInterval(counter);
+        restartGame();
+        return;
+      }
+    }, 1000);
+  }
 };
 </script>
