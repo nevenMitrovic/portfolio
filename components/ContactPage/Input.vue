e6a08895300635b39be597fda6a957aa2030eb97
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-12-28 17:23:35 +0100

    fix(deploy): problem with input v-model compiler

--- components/ContactPage/Input.vue
+++ components/ContactPage/Input.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="flex flex-col">
     <label :for="name" class="text-base_true_gray text-base mb-1">
-        {{ label }}:
+      {{ label }}:
     </label>
     <input
       :id="id"
-      :value="modelValue"
-      @input="handleInput(($event.target as HTMLInputElement).value)"
+      :value="inputValue"
+      :name="name"
+      @input="handleChange"
+      @blur="handleBlur"
       type="text"
       class="border border-lines bg-transparent w-full py-2 rounded-md focus:outline-none focus:ring-0 focus:shadow-md focus:shadow-base_true_gray/30 text-base_true_gray px-4"
     />
+    <p class="help-message pt-2" v-show="errorMessage || meta.valid">
+      {{ errorMessage || successMessage }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { useField } from "vee-validate";
+
+const props = defineProps({
   name: { type: String, required: true },
   label: { type: String, required: true },
   id: { type: String, required: true },
   modelValue: { type: String, required: true },
+  successMessage: {
+    type: String,
+    default: "",
+  },
 });
 
 const emits = defineEmits(["update:modelValue"]);
-const handleInput = (value: string) => {
-  emits("update:modelValue", value);
-};
+
+const name = toRef(props, "name");
+const {
+  value: inputValue,
+  errorMessage,
+  handleBlur,
+  handleChange,
+  meta,
+} = useField(name, undefined, {
+  initialValue: props.modelValue,
+});
 </script>
